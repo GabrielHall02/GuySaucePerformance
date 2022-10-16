73d7c0a128a7b882eb7ea972ff631582982ec6ed
GabrielHall02 <ghallabreu@gmail.com>
2022-10-16 12:35:45 +0100

    Creating loader

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   } from "react-router-dom";
 
 import "./App.css";
+import "./loader.css"
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
